ac9d1f3d367f7ef9f2cf2a3820472f7a34971f8c
Peyman Majidi <peyman.majidi@gmail.com>
2020-10-13 10:17:22 +0330

    formating the code

--- cnc.py
+++ cnc.py
@@ -2,21 +2,18 @@ import time
 import pyautogui
 import keyboard
 
-
-# pyautogui.confirm(text='HELLO', title='', buttons=['OK', 'Cancel'])
-
-time.sleep(5)
-distance = 500
+time.sleep(5) # execute paint
+distance = 300
 while distance > 0:
         if keyboard.is_pressed('p'):
                 while True:
-                      if keyboard.is_pressed('r'):
-                              break
+                        if keyboard.is_pressed('r'):
+                                break
 
-        pyautogui.drag(distance, 0, duration=0.1)   # move right
-        distance -= 5
-        pyautogui.drag(0, distance, duration=0.1)   # move down
-        pyautogui.drag(-distance, 0, duration=0.1)  # move left
-        distance -= 5
+        pyautogui.drag(distance, 0, duration=0.2)   # move right
+        distance -= 10
+        pyautogui.drag(0, distance, duration=0.2)   # move down
+        pyautogui.drag(-distance, 0, duration=0.2)  # move left
+        distance -= 10
         pyautogui.drag(0, -distance, duration=0.1)  # move up
 
